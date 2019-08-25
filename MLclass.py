f1e68f4a5d1e91487b4ce27c19414d704d7321b1
jmpb1998 <46171230+jmpb1998@users.noreply.github.com>
2019-08-25 19:15:24 +0100

    Add files via upload

---
+++ MLclass.py
@@ -0,0 +1,73 @@
+import os
+
+import csv
+#import inline as inline
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn import svm
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.metrics import classification_report, confusion_matrix
+import socketio
+import json
+from warnings import simplefilter
+
+simplefilter(action='ignore', category=FutureWarning)
+
+try:
+    sio = socketio.Client()
+    sio.connect('http://e3c85a63.ngrok.io')
+except:
+    pass
+
+
+
+#This is Iris data set, input own dataset
+iris = pd.read_csv('Data_Final.csv', dtype=object)
+
+sns.pairplot(data=iris,vars=["Red","Orange","Yellow","Green","Blue","Violet"], hue='concentration', palette='Set2')
+# data is data, hue is different colors, pallete are colors for hue
+
+#plt.show()
+
+with open('Data_Final.csv', newline='') as csvfile:
+    data = list(csv.reader(csvfile))
+
+print(data)
+
+clf = svm.SVC(gamma='0.0001', C=0.0001)
+# C is classification margin, gamma is radius of influence of model chosen support vectors
+x = iris.iloc[:, :-1]
+y = iris.iloc[:, 6]
+print(x)
+print(y)
+#x_test = [iris.iloc[149, :-1]]
+#y_test = [iris.iloc[149, 4]]
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25)
+#Test Size is the E(out) percentage with regards to E(total)
+model=SVC(kernel='rbf', C = 0.0001)
+# Use RBF if nonlinear, linear is linear
+model.fit(x_train, y_train)
+
+pred = model.predict(x_test)   # Predict
+
+#print('Prediction: ' + pred[0])
+'''
+with open('data.json', 'w') as datajson:
+    json.dump({'concentration': pred[0]}, datajson)
+
+with open('user.json', 'r') as useremail:
+    emailjson = json.load(useremail)
+    print(emailjson)
+    sio.emit('pi2database', {'concentration': pred[0], 'email': emailjson['email']})
+
+
+sio.disconnect()
+
+'''
+print(confusion_matrix(y_test,pred))    # Correctness matrix
+
+print(classification_report(y_test, pred))  # Display correctness
