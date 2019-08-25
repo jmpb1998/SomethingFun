bc9085ee470d8accf2dd8c31394de515d7c28fe5
catleonidas <37508051+catleonidas@users.noreply.github.com>
2019-08-25 04:27:04 +0100

    Add files via upload

--- SVMIris.py
+++ SVMIris.py
@@ -1,7 +1,7 @@
 import os
 
 import csv
-import inline as inline
+#import inline as inline
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -10,6 +10,15 @@ from sklearn import svm
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from sklearn.metrics import classification_report, confusion_matrix
+import socketio
+import json
+from warnings import simplefilter
+
+simplefilter(action='ignore', category=FutureWarning)
+
+sio = socketio.Client()
+sio.connect('http://e3c85a63.ngrok.io')
+
 
 
 #This is Iris data set, input own dataset
@@ -24,7 +33,7 @@ with open('iris.csv', newline='') as csvfile:
     data = list(csv.reader(csvfile))
 #print(data)
 
-clf = svm.SVC(gamma=0.0001, C=1)
+clf = svm.SVC(gamma='scale', C=1)
 # C is classification margin, gamma is radius of influence of model chosen support vectors
 x_train = iris.iloc[:, :-1]
 y_train = iris.iloc[:, 4]
@@ -33,7 +42,7 @@ x_test = [iris.iloc[149, :-1]]
 
 #x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.6)
 #Test Size is the E(out) percentage with regards to E(total)
-model=SVC(kernel='rbf')
+model=SVC(kernel='rbf', C = 1)
 # Use RBF if nonlinear, linear is linear
 model.fit(x_train, y_train)
 
@@ -41,6 +50,16 @@ pred = model.predict(x_test)   # Predict
 
 print('Prediction: ' + pred[0])
 
+with open('data.json', 'w') as datajson:
+    json.dump({'concentration': pred[0]}, datajson)
+
+with open('user.json', 'r') as useremail:
+    emailjson = json.load(useremail)
+    print(emailjson)
+    sio.emit('pi2database', {'concentration': pred[0], 'email': emailjson['email']})
+
+
+sio.disconnect()
 #print(confusion_matrix(y_test,pred))    # Correctness matrix
 
 #print(classification_report(y_test, pred))  # Display correctness
