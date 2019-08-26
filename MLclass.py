f513061e900abfc79fd4ee1c8a312d06e6401b72
catleonidas <37508051+catleonidas@users.noreply.github.com>
2019-08-26 14:25:16 +0100

    Add files via upload

--- MLclass.py
+++ MLclass.py
@@ -13,6 +13,13 @@ from sklearn.metrics import classification_report, confusion_matrix
 import socketio
 import json
 from warnings import simplefilter
+import as7262
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+GPIO.setup(10,GPIO.OUT)
+GPIO.output(10, GPIO.HIGH)
 
 simplefilter(action='ignore', category=FutureWarning)
 
@@ -21,6 +28,51 @@ try:
     sio.connect('http://e3c85a63.ngrok.io')
 except:
     pass
+
+as7262.soft_reset()
+as7262.set_gain(64)
+as7262.set_integration_time(17.857)
+as7262.set_measurement_mode(2)
+as7262.set_illumination_led(0)
+
+totred = 0
+totorange = 0
+totyellow = 0
+totgreen = 0
+totblue = 0
+totviolet = 0
+
+try:
+    for i in range(50):
+        values = as7262.get_calibrated_values()
+        print("""
+Red:    {}
+Orange: {}
+Yellow: {}
+Green:  {}
+Blue:   {}
+Violet: {}""".format(*values))
+        totred += values.red
+        totorange += values.orange
+        totyellow += values.yellow
+        totgreen += values.green
+        totblue += values.blue
+        totviolet += values.violet
+
+    totred = totred / 50
+    totorange = totorange / 50
+    totyellow = totyellow / 50
+    totgreen = totgreen / 50
+    totblue = totblue / 50
+    totviolet = totviolet / 50
+
+except KeyboardInterrupt:
+    as7262.set_measurement_mode(3)
+
+
+x_testset = [{'Red': totred, 'Orange': totorange, 'Yellow': totyellow, 'Green':totgreen, 'Blue': totblue, 'Violet': totviolet}]
+
+x_test = pd.DataFrame(x_testset)
 
 
 
@@ -39,14 +91,12 @@ print(data)
 
 clf = svm.SVC(gamma='0.0001', C=0.0001)
 # C is classification margin, gamma is radius of influence of model chosen support vectors
-x = iris.iloc[:, :-1]
-y = iris.iloc[:, 6]
-print(x)
-print(y)
+x_train = iris.iloc[:, :-1]
+y_train = iris.iloc[:, 6]
 #x_test = [iris.iloc[149, :-1]]
 #y_test = [iris.iloc[149, 4]]
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25)
+#x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25)
 #Test Size is the E(out) percentage with regards to E(total)
 model=SVC(kernel='rbf', C = 0.0001)
 # Use RBF if nonlinear, linear is linear
@@ -54,8 +104,8 @@ model.fit(x_train, y_train)
 
 pred = model.predict(x_test)   # Predict
 
-#print('Prediction: ' + pred[0])
-'''
+print('Prediction: ' + pred[0])
+
 with open('data.json', 'w') as datajson:
     json.dump({'concentration': pred[0]}, datajson)
 
@@ -67,7 +117,6 @@ with open('user.json', 'r') as useremail:
 
 sio.disconnect()
 
-'''
-print(confusion_matrix(y_test,pred))    # Correctness matrix
+#print(confusion_matrix(y_test,pred))    # Correctness matrix
 
-print(classification_report(y_test, pred))  # Display correctness
+#print(classification_report(y_test, pred))  # Display correctness
